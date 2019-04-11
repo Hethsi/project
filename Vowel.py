4cf017cb7b0ab71462466f9812df4bf83f2ec146
Hethsi <49511441+Hethsi@users.noreply.github.com>
2019-04-11 16:39:43 +0530

    Vowel.py

---
+++ Vowel.py
@@ -0,0 +1,5 @@
+chr=input("Enter A Char")
+if(chr=='A' or chr=='a' or chr=='E' or chr=='e' or chr=='I' or chr=='i' or chr=='O' or chr=='o' or chr='U' or chr=='u'):
+print(chr."Is a Vowel")
+else:
+print(chr."Is Consonant")
