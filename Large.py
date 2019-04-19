9d1417c50588f1436816fa9feaf35d72dfd9fa58
Hethsi <49511441+Hethsi@users.noreply.github.com>
2019-04-19 07:54:48 -0700

    Create Large.py

---
+++ Large.py
@@ -0,0 +1,10 @@
+n1=int(input("Enter N1")
+n2=int(input("Enter N2")
+n3=int(input("Enter N3")
+if(n1>=num2) and (num1>=num3):
+largest=n1
+elif(num2>=num1) and (num2>=num3):
+largest=n2
+else:
+largest=n3
+print("The Largest Number Bt n1,n2 and n3 is",largest)
