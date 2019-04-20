72adc5c47a6a8d36fde46236977db4569bf89bd5
Hethsi <49511441+Hethsi@users.noreply.github.com>
2019-04-20 01:54:17 -0700

    Large1.py

---
+++ Large1.py
@@ -0,0 +1,8 @@
+n1=int(input("Enter First Number"))
+n2=int(input("Enter Second Number"))
+n3=int(input("Enter Tird Number"))
+if(n1>n2) and (n1>n3):
+ largest=n2
+else:
+ largest=n3
+print("The Largest Number Is",largest)
