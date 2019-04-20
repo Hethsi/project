0c713207a2e54424905374a032efd4c54c35385f
Hethsi <49511441+Hethsi@users.noreply.github.com>
2019-04-20 05:58:14 -0700

    Sorted.py

---
+++ Sorted.py
@@ -0,0 +1,9 @@
+port numpy as np
+f selection_sort(N):
+ for i in range(len(N)):
+  swap=i+np.argmin(N[i:])
+  
+  (N[i],x[swap])=(N[swapp,N[i])
+ return N
+N=np.array[2,1,4,3,5])
+selection_sort(N)
