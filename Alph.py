900607996128c8f16758aaaa3fd740b2b0ddd3aa
Hethsi <49511441+Hethsi@users.noreply.github.com>
2019-04-20 02:02:23 -0700

    Alph.py

---
+++ Alph.py
@@ -0,0 +1,9 @@
+print("Enter '0' for exit")
+c=input("Enter Any Character:");
+if c==0:
+ exit();
+else:
+ if((c>='a' and c<='z') or ch>='A' and c<='Z')):
+  print(c,"Alphabet")
+ else:
+  print(c,"Not")
