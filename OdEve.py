5667d27716a26df501bad196d5829fd1e2a0cac1
Hethsi <49511441+Hethsi@users.noreply.github.com>
2019-04-22 06:33:40 -0700

    OdEve.py

---
+++ OdEve.py
@@ -0,0 +1,10 @@
+N=int(input("Enter a number:"))
+M=int(input("Enter a number:")
+if(N%2)==0:
+ print("Is Even")
+elif:
+ print("It is odd")
+elif(M%2)==0:
+ print("Is Even")
+else:
+ print("It is odd)
