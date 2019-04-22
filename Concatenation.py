d94bfc03710487121aebb79a163db70f24711635
Hethsi <49511441+Hethsi@users.noreply.github.com>
2019-04-22 03:08:38 -0700

    Concatenation.py

---
+++ Concatenation.py
@@ -0,0 +1,10 @@
+s1=input("Enter the first string:")
+s2=input("Enter the second string:")
+s3=s1+s2
+print("\n String after concatenation=",s3)
+print("s1=",s1)
+print("s2=",s2)
+print("s3=",s3)
+print("Enter 'x' for exit")
+if s1=='x':
+ exit()
