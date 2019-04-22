d22cb28f0d1f1891109644991a54f59fa694cbd2
Hethsi <49511441+Hethsi@users.noreply.github.com>
2019-04-22 02:08:31 -0700

    Swapping.py

---
+++ Swapping.py
@@ -0,0 +1,9 @@
+n1=input('Enter First Number:")
+n2=input('Enter Second Number:")
+print("Original Value of n1:",n1)
+print("Original Vlaue of n2:",n2)
+temp=n1
+n1=n2
+n2=temp
+print("Swapping Value of n1:",n1)
+print("Swapping Value of n2:",n2)
