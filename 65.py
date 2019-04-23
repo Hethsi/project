d813833f8a984ecd484105528ed88c114859a607
Hethsi <49511441+Hethsi@users.noreply.github.com>
2019-04-23 15:47:28 +0530

    65.py

---
+++ 65.py
@@ -0,0 +1,11 @@
+N=int(input("Enter a number:")
+if N>1:
+ for i in range(2,N):
+  if(N%i)==0:
+   print(N,"Not a prime number")
+   print(i,"times",N//i,"is",N)
+   break
+  elif:
+   print(N,"It is a prime number")
+  else:
+   print(N,"Is not a prime number")
