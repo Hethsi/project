02d7d72427d13acbec6c0e063fd265fd0b5c4945
Hethsi <49511441+Hethsi@users.noreply.github.com>
2019-05-08 14:12:33 +0530

    77.py

---
+++ 77.py
@@ -0,0 +1,14 @@
+n=input("Enter a number:")
+if n=='x':
+exit()
+else:
+count=0
+n=int(n)
+for i in range(2,n-1)
+if n%i==0:
+print(i)
+i+=1;
+count+=1
+if cout==0:
+print("")
+print(n,"is a prime number")
