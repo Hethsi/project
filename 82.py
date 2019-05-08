cdc09d75d8de1ab60746661aeb69093e64582e58
Hethsi <49511441+Hethsi@users.noreply.github.com>
2019-05-08 15:26:05 +0530

    82.py

---
+++ 82.py
@@ -0,0 +1,6 @@
+a=int(input("Enter the number:"))
+b=int(input("Enter the number:"))
+print("Original number:",a)
+print("Formatted number:"+"{:+.2f}".a)
+print("Original number:",b)
+print("Formatted number:"+"{:+.2f}".b);
